01bb74cd33f0529184814181d237903ef51257cb
cntod <67031527+cntod@users.noreply.github.com>
2020-07-08 17:16:41 +0900

    chu knock

--- Assets/02.Scirpts/Chu/Knock.cs
+++ Assets/02.Scirpts/Chu/Knock.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class Knock : MonoBehaviour
 {
     AudioSource knockWall;
+    AudioSource knockRoomSound;
+    AudioSource knockRoomSound2;
+    bool knockRoom = false;
+    int count = 0;
     private void Awake()
     {
-        
-            knockWall = this.GetComponent<AudioSource>();
+        knockWall = this.GetComponent<AudioSource>();
+        knockRoomSound = GameObject.Find("KnockRoom").GetComponent<AudioSource>();
+        knockRoomSound2 = GameObject.Find("KnockRoom2").GetComponent<AudioSource>();
+            
     }
-    private void OnCollisionEnter(Collision other)
+    private void Start()
     {
-        
+        NextScene();
     }
+    
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Hand") ){
             knockWall.Play();
-            print("녹");
+            count += 1;
+            if(knockRoom == false)
+            {
+                count = 0;
+            }
+            else if(knockRoom == true && count >=4 )
+            {
+                count = 0;
+                knockRoomSound.Play();
+
+            }
         }
+    }
+    public void NextScene()
+    {
+        knockRoom = true;
+        Invoke("KnockRoom",15f);
+        Invoke("KnockRoom",20f);
+    }
+    void KnockRoom()
+    {
+        knockRoomSound2.Play();
     }
 }
