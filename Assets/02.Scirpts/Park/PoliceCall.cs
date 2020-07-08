34e9698dea1ff130b1b11740a0b3cb82bed14acf
Yuzinida <67031479+Yuzinida@users.noreply.github.com>
2020-07-08 13:12:47 +0900

    yujin

---
+++ Assets/02.Scirpts/Park/PoliceCall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceCall : MonoBehaviour
+{
+    public PoliceMove policeMove;
+
+    void OnTriggerEnter(Collider other) 
+	{
+		if (other.CompareTag ("Player"))
+		{
+            policeMove.enabled = true;
+
+        }
+
+    }
+}
---
+++ Assets/02.Scirpts/Park/PoliceMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceMove : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 direction;
+    public float speed = 3;
+    public float rotSpeed = 5;
+
+    //public float velocity;
+    //public float accelaration;
+
+    public void Update()
+    {
+        MoveToTarget();
+    }
+    public void MoveToTarget()
+    {
+        GameObject policecall = GameObject.FindWithTag("PoliceCome");
+        target = policecall.transform;
+
+
+        direction = (target.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+
+        GetComponent<CharacterController>().SimpleMove(direction.normalized * speed); 
+
+
+        //accelaration = 0.1f;
+        //velocity = (velocity + accelaration * Time.deltaTime);
+        //float distance = Vector3.Distance(target.position, transform.position);
+        //if (distance <= 10.0f)
+        //{
+        //   this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
+        //                                           transform.position.y + (direction.y * velocity),
+        //                                             transform.position.z);
+        // }
+
+        //else
+        //{
+        //   velocity = 0.0f;
+        //}
+    }
+}
