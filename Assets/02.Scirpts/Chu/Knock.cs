db25bd24cdfdca0c890495f0e9a2f726fd02a0ef
bean <rndpqls94@naver.com>
2020-07-09 17:09:10 +0900

    KOO : 0709 깃 날라간 컴포넌트 다시 붙임

--- Assets/02.Scirpts/Chu/Knock.cs
+++ Assets/02.Scirpts/Chu/Knock.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -80,4 +80,4 @@ public class Knock : MonoBehaviour
         reStartSing.Play();
     }
 
-}
+}*/
