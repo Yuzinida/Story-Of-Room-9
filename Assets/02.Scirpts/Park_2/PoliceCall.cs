20cf59fddb675ae4bb30cc72f54259b73a9a9d45
Yuzinida <67031479+Yuzinida@users.noreply.github.com>
2020-07-09 14:40:10 +0900

    라이팅 테스트입니다
    aura 테스트를 했는데... 적용 테스트를 다시 해야 합니다

---
+++ Assets/02.Scirpts/Park_2/PoliceCall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceCall : MonoBehaviour
+{
+    public PoliceMove policeMove;
+
+    void OnTriggerEnter(Collider other) 
+	{
+		if (other.CompareTag ("Player"))
+		{
+            Debug.Log("111");
+            policeMove.enabled = true;
+
+        }
+
+    }
+}
