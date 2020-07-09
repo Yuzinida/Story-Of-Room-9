cef4e60da640dcb5f582aa3bfdfd0991ffe2567e
Yuzinida <67031479+Yuzinida@users.noreply.github.com>
2020-07-09 18:28:41 +0900

    Yujin_9번방 수정
    포스트 프로세싱 효과 추가
    먼지 이펙트 추가 (추후 수정 예정)

--- Assets/02.Scirpts/Park/PoliceMove.cs
+++ Assets/02.Scirpts/Park/PoliceMove.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -9,17 +9,10 @@ public class PoliceMove : MonoBehaviour
     public Transform target;
     public Vector3 direction;
 
-    public AudioSource audioSource;
+
     public float speed = 3;
     public float rotSpeed = 5;
-
-    //public float velocity;
-    //public float accelaration;
-
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
+    public AudioSource policesound;
 
     public void Update()
     {
@@ -37,13 +30,6 @@ public class PoliceMove : MonoBehaviour
         GetComponent<CharacterController>().SimpleMove(direction.normalized * speed);
 
         //오브젝트가 지역에 도착하고 3초 후에 다시 원래 자리로 돌아간다
-
     }
 
-
-    //위 행동을 할 때 오디오 소스가 나온다
-    void AUdioCall()
-    {
-        audioSource.Play();
-    }
 }
