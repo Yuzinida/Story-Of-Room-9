3ab30de931dc92e9aad6fd445c02d30ba5955dd3
cntod <67031527+cntod@users.noreply.github.com>
2020-07-09 12:57:55 +0900

    9호실작업중 itween 추가

--- Assets/02.Scirpts/Chu/Knock.cs
+++ Assets/02.Scirpts/Chu/Knock.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Knock : MonoBehaviour
 {
-    AudioSource knockWall;
-    AudioSource knockRoomSound;
-    AudioSource knockRoomSound2;
+    AudioSource knockWall, knockRoomSound, knockRoomSound2, arirangAndStop, stopSinging,reStartSing;
     bool knockRoom = false;
     int count = 0;
     private void Awake()
@@ -14,13 +12,20 @@ public class Knock : MonoBehaviour
         knockWall = this.GetComponent<AudioSource>();
         knockRoomSound = GameObject.Find("KnockRoom").GetComponent<AudioSource>();
         knockRoomSound2 = GameObject.Find("KnockRoom2").GetComponent<AudioSource>();
+        arirangAndStop = GameObject.Find("ArirangAndStop").GetComponent<AudioSource>();
+        stopSinging = GameObject.Find("StopSinging").GetComponent<AudioSource>();
+        reStartSing = GameObject.Find("ReStartSing").GetComponent<AudioSource>();
             
     }
-    private void Start()
-    {
-        NextScene();
-    }
-    
+    // //테스트
+    //     private void Start()
+    //     {
+    //         NextScene();
+    //         Recede();
+    //     }
+    // //
+
+    //노크 이벤트
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Hand") ){
@@ -38,14 +43,42 @@ public class Knock : MonoBehaviour
             }
         }
     }
+
+    //배식 쪽지 이벤트부터 씬 이야기 진행
     public void NextScene()
     {
         knockRoom = true;
         Invoke("KnockRoom",15f);
         Invoke("KnockRoom",20f);
+        arirangAndStop.Play();
+        Invoke("StopSinging",52f);
     }
+    // // 플레이어에게 노크 사인주는
     void KnockRoom()
     {
         knockRoomSound2.Play();
     }
+    // // 아리랑 노래 끊고, 멀어지는 순사, 노크 반응차단
+    void StopSinging()
+    {
+        stopSinging.Play();
+        knockRoom = false;
+        Invoke("Recede,ReStartSing",8f);
+    }
+    // // 발걸음 멀어지는 코드
+    void Recede()
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("time",3.0f);
+        ht.Add("path",iTweenPath.GetPath("Walk"));
+        ht.Add("easetype",iTween.EaseType.linear);
+
+        iTween.MoveTo(stopSinging.gameObject, ht);
+    }    
+    // // 발걸음 멀어지는 코드와 동시에 다시 개기듯 노래 또 부르기 시작하는 장면
+    void ReStartSing()
+    {
+        reStartSing.Play();
+    }
+
 }
