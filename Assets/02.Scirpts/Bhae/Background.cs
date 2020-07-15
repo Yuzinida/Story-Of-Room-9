7825321ce4d5440a3960587f13c1741d63e46750
102real <67042750+102real@users.noreply.github.com>
2020-07-15 12:58:42 +0900

    Bhae : Dof 추가

---
+++ Assets/02.Scirpts/Bhae/Background.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public Material material;
+    public float scrollSpeed = 0.2f;
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Vector2.up;
+
+        material.mainTextureOffset += direction * scrollSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/02.Scirpts/Bhae/Dof.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Dof : MonoBehaviour
+{
+    public PostProcessVolume volume;
+    private DepthOfField _DepthOfField;
+    public float dof = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+            volume.profile.TryGetSettings(out _DepthOfField);
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _DepthOfField.focalLength.value = dof;
+    }
+}
