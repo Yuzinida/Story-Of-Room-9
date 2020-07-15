f024798cadb55a835467cf79dabd5d5094734a51
bean <rndpqls94@naver.com>
2020-07-15 18:42:42 +0900

    Koo : DRoom / DRoom_Koo1 수정
    대화창 흐느낌 소리 부분 수정 (완성전)

--- Assets/02.Scirpts/Koo/DRoom_Dialogue.cs
+++ Assets/02.Scirpts/Koo/DRoom_Dialogue.cs
@@ -9,17 +9,7 @@ public class DRoom_Dialogue : MonoBehaviour
     public GameObject PlayerUI_1, PlayerUI_2, PlayerUI_3, PlayerUI_4;
     public GameObject YouUI;
     public AudioSource You_Sound1, You_Sound2, You_Sound3, You_Sound4;
-
-    // public Text Text_You1_1;
-    // public Text Text_You2_1;
-    // public Text Text_You2_2;
-    // public Text Text_You2_3;
-    // public Text Text_You3_1;
-    // public Text Text_You3_2;
-    // public Text Text_You4_1;
-    
     public Text Text_You;
-
 
     private void Awake()
     {
@@ -31,24 +21,6 @@ public class DRoom_Dialogue : MonoBehaviour
 
         // You UI와 판넬 입력
         YouUI = GameObject.Find("Canvas_YouUI");
-
-
-        // // You Text 입력
-        // Text_You1_1.GetComponent<Text>().text 
-        //             = "관순 : ... 저는 의무라고만 여겼어요. \n 나라를 되찾으려는 당연한 의무...";
-        // Text_You2_1.GetComponent<Text>().text 
-        //             = "관순 : 나란 년은 정말 이상속에서 잘난 체만 하면서 산 거 같아요. ";
-        // Text_You2_2.GetComponent<Text>().text 
-        //             = "관순 : 진짜 만숙이 어머니 말 대로 내가 경성에서 만세소식을 전하러 내려오지 않았다면.. ";
-        // Text_You2_3.GetComponent<Text>().text 
-        //             = "관순 : 우리 부모님 고향사람들까지 돌아가시지 않았을지도 모르잖아요.  ";
-        // Text_You3_1.GetComponent<Text>().text 
-        //             = "관순 : 그래도 정당한일을 하니까 하나님이 도와 주실 줄 알았어요.";
-        // Text_You3_2.GetComponent<Text>().text 
-        //             = "관순 : 언니들에 비하면 나는 만세부를 자격도 없는데..";
-        // Text_You4_1.GetComponent<Text>().text 
-        //             = "관순 : 금년이 윤달이 아니라면 딱 3일 남은 거 같아요.";
-
 
 
         // 소리 입력
@@ -67,7 +39,7 @@ public class DRoom_Dialogue : MonoBehaviour
 
         YouUI.SetActive(false);
 
-        StartCoroutine(_typing());
+        //StartCoroutine(_typing());
 
     }
 
@@ -78,7 +50,6 @@ public class DRoom_Dialogue : MonoBehaviour
 
         YouUI.SetActive(true);
         Text_You.text = "관순 : ... 저는 의무라고만 여겼어요. 나라를 되찾으려는 당연한 의무...";
-        //StartCoroutine(_typing());
         You_Sound1.Play();
         Debug.Log("You_Sound1.Play()");
 
@@ -88,16 +59,17 @@ public class DRoom_Dialogue : MonoBehaviour
 
     public void OnClickPlayerUI_2()
     {
+        // UI 뜨는 것과 동시에 흐느끼는 소리(20초)가 나타나고 
+        // 20초 뒤에 UI를 클릭하지 않으면 자동으로 넘어간다.
         PlayerUI_2.SetActive(false);
 
         YouUI.SetActive(true);
-        Text_You.text = "관순 : 나란 년은 정말 이상속에서 잘난 체만 하면서 산 거 같아요.";
-        Invoke("Text_You2_2_func", 10.0f);
-        Invoke("Text_You2_3_func", 15.0f);
+        
+        Invoke("Text_You2_1_func", 25.0f); // 첫 번째 대사 UI // PLAYER UI 자동 없어지기
+        Invoke("Text_You2_2_func", 35.0f); // 두 번째 대사 UI
+        Invoke("Text_You2_3_func", 40.0f);  // 세 번째 대사 UI
 
 
-        You_Sound2.Play();
-        Debug.Log("You_Sound2.Play()");
         Invoke("PlayerUI_3_func", 52.0f);
     }
 
@@ -127,7 +99,10 @@ public class DRoom_Dialogue : MonoBehaviour
         Invoke("YouUIFalse_func", 9.0f);
     }
 
-
+    void Text_You2_1_func()
+    {
+        Text_You.text = "관순 : 나란 년은 정말 이상속에서 잘난 체만 하면서 산 거 같아요.";
+    }
     void Text_You2_2_func()
     {
         Text_You.text = "관순 : 진짜 만숙이 어머니 말 대로 내가 경성에서 만세소식을 전하러 내려오지 않았다면..";
@@ -158,6 +133,7 @@ public class DRoom_Dialogue : MonoBehaviour
     {
         PlayerUI_2.SetActive(true);
         YouUI.SetActive(false);
+        You_Sound2.Play(); // 흐느끼는 소리 미리 들리게 한다.
     }
     void PlayerUI_3_func()
     {
@@ -170,16 +146,16 @@ public class DRoom_Dialogue : MonoBehaviour
         YouUI.SetActive(false);
     }
 
-    public Text tx;
-    IEnumerator _typing()
-    {
-        yield return new WaitForSeconds(2f);
-        for (int i = 0; i <= Text_You.text.Length; i++)
-        {
-            Text_You.text = Text_You.text.Substring(0, i);
+    // public Text tx;
+    // IEnumerator _typing()
+    // {
+    //     yield return new WaitForSeconds(2f);
+    //     for (int i = 0; i <= Text_You.text.Length; i++)
+    //     {
+    //         Text_You.text = Text_You.text.Substring(0, i);
 
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
+    //         yield return new WaitForSeconds(0.1f);
+    //     }
+    // }
 
 }
