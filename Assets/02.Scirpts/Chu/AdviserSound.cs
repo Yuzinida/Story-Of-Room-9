30bab3651e62d60e6811d5064a91861301059a25
cntod <67031527+cntod@users.noreply.github.com>
2020-07-16 15:40:38 +0900

    chu_0716

---
+++ Assets/02.Scirpts/Chu/AdviserSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdviserSound : MonoBehaviour
+{
+    AudioSource dRoom;
+    void Start()
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("time",30f);
+        ht.Add("path",iTweenPath.GetPath("Adviser"));
+        ht.Add("easetype",iTween.EaseType.linear);
+
+        iTween.MoveTo(this.gameObject, ht);
+
+        dRoom = GameObject.Find("DRoom").GetComponent<AudioSource>();
+
+        StartCoroutine("DRoomSound");
+
+    }
+
+    IEnumerator DRoomSound()
+    {
+        yield return new WaitForSeconds(31f);
+
+        dRoom.Play();
+    }
+
+}
