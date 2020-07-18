e6f35836544130e302a2dae40713a9e151e6c6c7
Yuzinida <67031479+Yuzinida@users.noreply.github.com>
2020-07-18 17:10:38 +0900

    Park_독방 시간에 따른 라이트 조절

---
+++ Assets/02.Scirpts/Park_2/DroomLight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroomLight : MonoBehaviour
+{
+
+    Light myLight;
+    float interval = 0.3f;
+    float minAngle = 38.3f;
+    float maxAngle = 70f;
+    float flowTime;
+    public bool lightOn;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+        myLight.type = LightType.Spot;
+
+    }
+
+    void Update()
+    {
+//관순과의 대화 종료
+
+
+        if (lightOn)
+        {
+            flowTime = Mathf.Max(flowTime, 1);
+            flowTime += Time.deltaTime;
+            myLight.intensity = Mathf.PingPong(flowTime*38f, 100f);
+
+            // myLight.spotAngle = Random.Range(minAngle, maxAngle);
+
+            myLight.spotAngle = Mathf.Lerp(minAngle, maxAngle, flowTime);
+            // if (flowTime ==1){
+
+         //       DayChange();
+          // }
+
+        }
+
+    }
+
+    void DayChange()
+    {
+        maxAngle -= minAngle;
+    }
+}
+
