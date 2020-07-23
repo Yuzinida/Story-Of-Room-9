331e76f2c5433b2f2b22ae3aa73ca9cf830cf90a
cntod <67031527+cntod@users.noreply.github.com>
2020-07-23 10:10:01 +0900

    Chu_Fin

--- Assets/02.Scirpts/Bhae/Drow.cs
+++ Assets/02.Scirpts/Bhae/Drow.cs
@@ -8,16 +8,19 @@ public class Drow : MonoBehaviour
     public GameObject canvas1;
     public GameObject canvas2;
     GameObject sign;
+    DrawMgr draw;
     
     private void Start()
     {
         sign = GameObject.Find("Sign").transform.GetChild(0).gameObject;
+        draw = GameObject.Find("Controller (right)").GetComponent<DrawMgr>();
         StartCoroutine("Sign");
     }
 
     IEnumerator Sign()
     {
         yield return new WaitForSeconds(29f);
+        draw.enabled = true;
         sign.SetActive(true);
         canvas1.SetActive (false);
         canvas2.SetActive (true);
--- Assets/02.Scirpts/Chu/Sign.cs
+++ Assets/02.Scirpts/Chu/Sign.cs
@@ -11,7 +11,7 @@ public class Sign : MonoBehaviour
     Renderer scolor;
     Vector3 reposition;
     Transform target;
-    
+    AudioSource soundFadeout;
     public  GameObject name;
 
     private void Start()
@@ -29,7 +29,7 @@ public class Sign : MonoBehaviour
         if(other.CompareTag("Hand"))
         {
             line = name.transform;      
-            line.position = new Vector3(0.042f,0.15f,1.106f);
+            line.position = new Vector3(0.042f,0.15f,0.9f);
             line.rotation = Quaternion.Euler(25.6f,0,0);
             
             for(int i=0; i<line.childCount; i++)
@@ -56,7 +56,16 @@ public class Sign : MonoBehaviour
         //set and start fade to
         SteamVR_Fade.Start(Color.black, 2f);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
+        soundFadeout = target.gameObject.GetComponent<AudioSource>();
+
+        for(int i = 0; i <100;i++)
+        {     
+            yield return new WaitForSeconds(0.03f);       
+            soundFadeout.volume -= 0.007f;
+        }        
+
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(8);
     }
 }
