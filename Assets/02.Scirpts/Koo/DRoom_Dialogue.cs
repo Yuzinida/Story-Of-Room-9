695e89de185533ef3ab1f238511eebdcf59c4898
cntod <67031527+cntod@users.noreply.github.com>
2020-09-24 17:11:23 +0900

    0924_Chu

--- Assets/02.Scirpts/Koo/DRoom_Dialogue.cs
+++ Assets/02.Scirpts/Koo/DRoom_Dialogue.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 public class DRoom_Dialogue : MonoBehaviour
 {
-    GameObject PlayerUI_1, PlayerUI_2, PlayerUI_3, PlayerUI_4;    
-    AudioSource You_Sound1, You_Sound2, You_Sound3, You_Sound4, backSound,gamemgr;
+    GameObject PlayerUI_1, PlayerUI_2, PlayerUI_3, PlayerUI_4, PlayerUI_5;    
+    AudioSource You_Sound1, You_Sound2, You_Sound3, You_Sound4,You_Sound5, backSound,gamemgr, thunder2, thunderImpact;
     GameObject YouUI;
     public Text Text_You;
 
@@ -19,6 +19,7 @@ public class DRoom_Dialogue : MonoBehaviour
         PlayerUI_2 = GameObject.Find("PlayerDialogue").transform.GetChild(1).gameObject;
         PlayerUI_3 = GameObject.Find("PlayerDialogue").transform.GetChild(2).gameObject;
         PlayerUI_4 = GameObject.Find("PlayerDialogue").transform.GetChild(3).gameObject;
+        PlayerUI_5 = GameObject.Find("PlayerDialogue").transform.GetChild(4).gameObject;
 
         // You UI와 판넬 입력
         YouUI = GameObject.Find("YouDialogue").transform.GetChild(0).gameObject;
@@ -29,9 +30,11 @@ public class DRoom_Dialogue : MonoBehaviour
         You_Sound2 = GameObject.Find("YouSound_2").GetComponent<AudioSource>();
         You_Sound3 = GameObject.Find("YouSound_3").GetComponent<AudioSource>();
         You_Sound4 = GameObject.Find("YouSound_4").GetComponent<AudioSource>();
+        You_Sound5 = GameObject.Find("YouSound_5").GetComponent<AudioSource>();
         backSound = GameObject.Find("BackSound").GetComponent<AudioSource>();
         gamemgr = GameObject.Find("GameManager").GetComponent<AudioSource>();
-        
+        thunder2 = GameObject.Find("Thunder2").GetComponent<AudioSource>();
+        thunderImpact = GameObject.Find("ThunderImpact").GetComponent<AudioSource>();
     }
 
 
@@ -40,8 +43,6 @@ public class DRoom_Dialogue : MonoBehaviour
     {
         PlayerUI_1.SetActive(false);
 
-        YouUI.SetActive(true);
-        Text_You.text = "관순 : ... 저는 의무라고만 여겼어요. 나라를 되찾으려는 당연한 의무...";
         You_Sound1.Play();
 
         StartCoroutine("PlayerUI_2_func");
@@ -52,6 +53,8 @@ public class DRoom_Dialogue : MonoBehaviour
     public void OnClickPlayerUI_2()
     {
         PlayerUI_2.SetActive(false);
+        You_Sound2.Play(); 
+        StartCoroutine("PlayerUI_3_func");
 
         // YouUI.SetActive(true);
         // Text_You.text = "관순 : 어떻게 보면.. 나란 년은 정말 이상 속에서 잘난 체만 하면서 산 거 같아요.";
@@ -67,7 +70,7 @@ public class DRoom_Dialogue : MonoBehaviour
         // Text_You.text = "관순 : 그래도 정당한 일을 하니까 하나님이 도와 주실 줄 알았어요.";
         // Invoke("Text_You3_2_func", 10.0f);
 
-        You_Sound3.Play();
+        
 
         StartCoroutine("PlayerUI_4_func");
     }
@@ -75,8 +78,9 @@ public class DRoom_Dialogue : MonoBehaviour
 
     public void OnClickPlayerUI_4()
     {
+        thunderImpact.Play();
+        thunder2.Stop();
         PlayerUI_4.SetActive(false);
-
         // YouUI.SetActive(true);
         
         StartCoroutine("EndUID");
@@ -105,50 +109,111 @@ public class DRoom_Dialogue : MonoBehaviour
     //     // YouUI.SetActive(false);
     // }
 
+    public void OnClickPlayerUI_5()
+    {
+        PlayerUI_5.SetActive(false);
+        You_Sound5.Play();
+
+        StartCoroutine("RealEndUID");
+        
+    }
+
 
     IEnumerator PlayerUI_2_func()
     {
-        yield return new WaitForSeconds(13.0f);
+        yield return new WaitForSeconds(2f);
+        YouUI.SetActive(true);
+        Text_You.text = "관순 : ... 전.. 병천에서 온 유관순이라고 합니다.";
+
+        yield return new WaitForSeconds(7f);
         Text_You.text = "";
         PlayerUI_2.SetActive(true);        
-        You_Sound2.Play(); // 흐느낌 소리 여기서부터 시작!
+        
+    }
+    IEnumerator PlayerUI_3_func()
+    {
+        yield return new WaitForSeconds(6f); 
+        Text_You.text = "관순 : 전 잔다르크처럼 나라를 구하는 소녀가 되고 싶었어요.";
 
-        yield return new WaitForSeconds(15f); //대사시작시간
-        if(PlayerUI_2.activeSelf==true)
-        {
-            PlayerUI_2.SetActive(false);
-        }
-        Text_You.text = "관순 : 어떻게 보면.. 나란 년은 정말 이상 속에서 잘난 체만 하면서 산 거 같아요.";
+        yield return new WaitForSeconds(9f);
+        Text_You.text = "관순 : 그 의무감으로 저도 노력하면 자유를 찾을 수 있다 생각했는데..";
 
-        yield return new WaitForSeconds(12f);
-        Text_You.text = "관순 : 진짜 만숙이 어머니 말 대로 내가 경성에서 만세소식을 전하러 내려오지 않았다면..";
+        yield return new WaitForSeconds(9f);
+        Text_You.text = "관순 : 오히려 모든걸 빼앗겨버렸어요.. 제 눈앞에서...";
 
-        yield return new WaitForSeconds(12f);
-        Text_You.text = "관순 : 우리 부모님, 고향사람들까지 돌아가시지 않았을지도 모르잖아요.";
+        yield return new WaitForSeconds(7f);
+        Text_You.text = "관순 : 어머니.. 아버지.. 마을사람들까지 그놈들이 전부..!";
 
-        yield return new WaitForSeconds(13.0f);
+        yield return new WaitForSeconds(9.0f);
         Text_You.text = "";
+        
+        yield return new WaitForSeconds(2.0f);
         PlayerUI_3.SetActive(true);
     }
     IEnumerator PlayerUI_4_func()
     {
-        Text_You.text = "관순 : 그래도 정당한 일을 하니까 하나님이 도와 주실 줄 알았어요.";
+        yield return new WaitForSeconds(1f); 
+        You_Sound3.Play();
+
+        yield return new WaitForSeconds(1f); 
+        Text_You.text = "관순 : 저희 부모님 억울해서 어쩌죠  ";
         
-        yield return new WaitForSeconds(8.0f); 
-        Text_You.text = "관순 : 언니들에 비하면 나는 만세부를 자격도 없는데..";
+        yield return new WaitForSeconds(3.0f); 
+        Text_You.text = "관순 : 죽은 우리 마을 사람들 원통하고 분해서 어떡해요..";
+
+        yield return new WaitForSeconds(6.0f); 
+        Text_You.text = "관순 : 기도하며 행하면.. 하나님이 지혜를 주실 줄 알았는데..";
         
-        yield return new WaitForSeconds(10.0f); 
+        yield return new WaitForSeconds(1.5f); 
+        thunder2.Play();
+
+        yield return new WaitForSeconds(5.5f);
         Text_You.text = "";
+
+        yield return new WaitForSeconds(8.5f); 
         PlayerUI_4.SetActive(true);
         // YouUI.SetActive(false);
     }
     IEnumerator EndUID()
     {
-        yield return new WaitForSeconds(3.5f);
-        Text_You.text = "관순 : 금년이 윤달이 아니라면 딱 삼 일 남은 거 같아요.";
+        yield return new WaitForSeconds(7f);
         You_Sound4.Play();
 
-        yield return new WaitForSeconds(8.0f);
+        yield return new WaitForSeconds(2f);
+        Text_You.text = "관순 : 내가 원하는 것에 목숨을 바치는 거... 자유..";
+
+        yield return new WaitForSeconds(7.0f);
+        Text_You.text = "";
+
+        yield return new WaitForSeconds(3f);
+        Text_You.text = "관순 : 전 이상속에서 분노 하나에 빠져 떼만 쓰고 있었네요..";        
+        
+        yield return new WaitForSeconds(4.5f);
+        Text_You.text = "관순 : 뺏지말라고... 돌려내라고.. 이미 마음이 꺾인싸움..";
+        
+        yield return new WaitForSeconds(6f);
+        Text_You.text = "관순 : 내 자유를 그들 손에 쥐여주고 있었네 내가...";
+        
+        yield return new WaitForSeconds(4f);
+        Text_You.text = "관순 : 정작 나는 의무감만 있고 모든 걸 다 던질 생각조차 못 하고 있었어요.";
+        
+        yield return new WaitForSeconds(8.5f);
+        Text_You.text = "";
+        
+        yield return new WaitForSeconds(4f);
+        PlayerUI_5.SetActive(true);
+        
+    }
+
+    IEnumerator RealEndUID()
+    {
+        yield return new WaitForSeconds(5.5f);
+        Text_You.text = "관순 : 3월1일..";
+
+        yield return new WaitForSeconds(4.0f);
+        Text_You.text = "관순 : 네 이제 딱 3일 남은 거 같아요.";
+
+        yield return new WaitForSeconds(4.0f);
         YouUI.SetActive(false);
 
         yield return new WaitForSeconds(1.5f);
@@ -159,4 +224,6 @@ public class DRoom_Dialogue : MonoBehaviour
         }
         gamemgr.gameObject.GetComponent<DRoom_mgr>().Ending();
     }
+
+    
 }
