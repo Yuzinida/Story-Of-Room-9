364ef5a68a8a6a5fa314445f2ef585afd97cf74b
bean <rndpqls94@naver.com>
2020-09-28 16:37:37 +0900

    Koo : ROOM9 컨트롤러회전 / START 튜토리얼순차적 발생
    ROOM9 : 컨트롤러 45도 단위 회전 → 연속적으로 회전
    START : 플레이어컨트롤러 이동, 회전 추가,
    튜토리얼1,2,3이 순차적으로 나타나도록 설정

---
+++ Assets/02.Scirpts/Koo/Tutorial1to2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial1to2 : MonoBehaviour
+{
+    //public UIFADE fadeUI;
+    //Transform touchTwo;
+    //Transform player;
+    //GodSound godSound;
+
+    public GameObject Tutorial2;
+
+
+    private void Start()
+    {
+        //touchTwo = GameObject.Find("Touch_two").GetComponent<Transform>();
+        //godSound = GameObject.Find("GodSounds").GetComponent<GodSound>();
+        Tutorial2 = GameObject.Find("Tutorial2------------------").transform.GetChild(0).gameObject;
+    }
+    private void Update()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        //touchTwo.LookAt(player);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            Debug.LogFormat("컨트롤러가 원 안에 들어왔다");
+            Invoke("ShowTutorial2", 0.5f);
+            Debug.LogFormat("튜토리얼2 생성!");  // x 위치가 0보다 크면 1 작으면 -1이 나온다.
+
+        }
+    }
+
+    void ShowTutorial2() // 튜토리얼3가 나타난다.
+    {
+        Tutorial2.SetActive(true);
+    }
+
+
+
+}
---
+++ Assets/02.Scirpts/Koo/Tutorial2to3.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial2to3 : MonoBehaviour
+{
+    public GameObject Tutorial1;
+    public GameObject Tutorial2;
+    public GameObject Tutorial3;
+
+
+    private void Start()
+    {
+        //touchTwo = GameObject.Find("Touch_two").GetComponent<Transform>();
+        //godSound = GameObject.Find("GodSounds").GetComponent<GodSound>();
+        Tutorial1 = GameObject.Find("Tutorial1------------------").transform.GetChild(0).gameObject;
+        Tutorial2 = GameObject.Find("Tutorial2------------------").transform.GetChild(0).gameObject;
+        Tutorial3 = GameObject.Find("Tutorial3------------------").transform.GetChild(0).gameObject;
+    }
+
+    public void OnClick()
+    {
+        Debug.LogFormat("유아이클릭");
+        Invoke("ShowTutorial3", 0.5f);
+        Debug.LogFormat("튜토리얼3 생성!");
+
+        Invoke("DeleteTutorial1", 0.5f);
+        Invoke("DeleteTutorial2", 0.5f);
+        Debug.LogFormat("튜토리얼1,2 없어짐!");
+    }
+
+
+
+    void DeleteTutorial1() // 튜토리얼1가 없어진다.
+    {
+        Tutorial1.SetActive(false);
+    }
+
+    void DeleteTutorial2() // 튜토리얼2가 없어진다.
+    {
+        Tutorial2.SetActive(false);
+    }
+
+    void ShowTutorial3() // 튜토리얼3가 나타난다.
+    {
+        Tutorial3.SetActive(true);
+    }
+
+
+
+}
